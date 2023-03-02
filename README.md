db717fa9c86cc9fbf959b8e57dc94cb6163c9044
JpLuuCH <jpluu@companieshouse.gov.uk>
2023-03-02 13:08:23 +0000

    initial skeleton application commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # psc-data-api
 Handles CRUD functions for PSC's
+
+# Requirements
+
+- [Java 11](https://www.oracle.com/uk/java/technologies/javase/jdk11-archive-downloads.html)
+- [Maven](https://maven.apache.org/download.cgi)
+- [Git](https://git-scm.com/downloads)
---
+++ src/main/java/uk/gov/companieshouse/pscdataapi/PscDataApiApplication.java
@@ -0,0 +1,13 @@
+package uk.gov.companieshouse.pscdataapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PscDataApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PscDataApiApplication.class, args);
+    }
+
+}
