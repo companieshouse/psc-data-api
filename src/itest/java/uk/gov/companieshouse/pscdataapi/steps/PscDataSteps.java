68ff2509d5a3db3d949cd34e6f32515f60459f92
kohareCH <kohare1@companieshouse.gov.uk>
2023-05-10 15:46:04 +0100

    [DNSD-1649] Added in integration test for processing old psc data

--- src/itest/java/uk/gov/companieshouse/pscdataapi/steps/PscDataSteps.java
+++ src/itest/java/uk/gov/companieshouse/pscdataapi/steps/PscDataSteps.java
@@ -9,6 +9,7 @@ import io.cucumber.java.en.When;
 import org.assertj.core.api.Assertions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -19,11 +20,19 @@ import org.springframework.http.ResponseEntity;
 import static org.assertj.core.api.Assertions.assertThat;
 import static uk.gov.companieshouse.pscdataapi.config.AbstractMongoConfig.mongoDBContainer;
 
+import uk.gov.companieshouse.api.psc.FullRecordCompanyPSCApi;
+import uk.gov.companieshouse.api.psc.Statement;
 import uk.gov.companieshouse.pscdataapi.config.CucumberContext;
+import uk.gov.companieshouse.pscdataapi.models.PscData;
+import uk.gov.companieshouse.pscdataapi.models.PscDocument;
 import uk.gov.companieshouse.pscdataapi.util.FileReaderUtil;
 import uk.gov.companieshouse.pscdataapi.repository.CompanyPscRepository;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Collections;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 public class PscDataSteps {
     private String contextId;
@@ -55,8 +64,44 @@ public class PscDataSteps {
         assertThat(restTemplate).isNotNull();
     }
 
+    @Given("a psc data record exists with notification id {string} and delta_at {string}")
+    public void psc_statement_exists_for_company_and_id_with_delta_at(String notifcationId, String deltaAt) throws IOException {
+        String pscDataFile = (FileReaderUtil.readFile("src/itest/resources/json/input/psc_data_api.json"));
+        PscData pscData = objectMapper.readValue(pscDataFile, PscData.class);
+
+        PscDocument document = new PscDocument();
+        document.setId(notifcationId);
+        document.setCompanyNumber(COMPANY_NUMBER);
+        document.setData(pscData);
+        document.setDeltaAt(deltaAt);
+        mongoTemplate.save(document);
+        assertThat(companyPscRepository.findById(notifcationId)).isNotEmpty();
+    }
+
     @When("I send a PUT request with payload {string} file with notification id {string}")
     public void i_send_psc_statement_put_request_with_payload(String dataFile, String notificationId) {
+        String data = FileReaderUtil.readFile("src/itest/resources/json/input/" + dataFile + ".json");
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+
+        this.contextId = "5234234234";
+        CucumberContext.CONTEXT.set("contextId", this.contextId);
+        headers.set("x-request-id", this.contextId);
+        headers.set("ERIC-Identity", "TEST-IDENTITY");
+        headers.set("ERIC-Identity-Type", "key");
+        headers.set("ERIC-Authorised-Key-Roles", "*");
+
+        HttpEntity request = new HttpEntity(data, headers);
+        String uri = "/company/{company_number}/persons-with-significant-control/{notfication_id}/full_record";
+        ResponseEntity<Void> response = restTemplate.exchange(uri, HttpMethod.PUT, request, Void.class, COMPANY_NUMBER, notificationId);
+
+        CucumberContext.CONTEXT.set("statusCode", response.getStatusCodeValue());
+    }
+
+    @When("I send a PUT request with payload {string} file for record with notification Id {string}")
+    public void i_send_psc_data_put_request_with_payload(String dataFile, String notificationId) throws IOException {
         String data = FileReaderUtil.readFile("src/itest/resources/json/input/" + dataFile + ".json");
 
         HttpHeaders headers = new HttpHeaders();
@@ -86,6 +131,13 @@ public class PscDataSteps {
     public void i_should_receive_status_code(Integer statusCode) {
         int expectedStatusCode = CucumberContext.CONTEXT.get("statusCode");
         Assertions.assertThat(expectedStatusCode).isEqualTo(statusCode);
+    }
+
+    @When("a record exists with id {string} and delta_at {string}")
+    public void psc_record_exists(String notificationId, String deltaAt) throws NoSuchElementException {
+        Assertions.assertThat(companyPscRepository.existsById(notificationId)).isTrue();
+        Optional<PscDocument> document = companyPscRepository.findById(notificationId);
+        Assertions.assertThat(companyPscRepository.findById(notificationId).get().getDeltaAt()).isEqualTo(deltaAt);
     }
 
     @After
