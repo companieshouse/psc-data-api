f42f6656ee86075decc1a306b853e2baef77df6a
SALMANCH007 <salhadad@companieshouse.gov.uk>
2023-12-01 17:20:31 +0000

    initial feature flag enabled test

--- src/test/java/uk/gov/companieshouse/pscdataapi/api/ResourceChangedApiServiceAspectFeatureFlagEnabledITest.java
+++ src/test/java/uk/gov/companieshouse/pscdataapi/api/ResourceChangedApiServiceAspectFeatureFlagEnabledITest.java
@@ -1,54 +1,51 @@
 package uk.gov.companieshouse.pscdataapi.api;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import uk.gov.companieshouse.api.InternalApiClient;
-import uk.gov.companieshouse.api.chskafka.ChangedResource;
-import uk.gov.companieshouse.api.error.ApiErrorResponseException;
 import uk.gov.companieshouse.api.handler.chskafka.PrivateChangedResourceHandler;
 import uk.gov.companieshouse.api.handler.chskafka.request.PrivateChangedResourcePost;
-import uk.gov.companieshouse.api.http.HttpClient;
-import uk.gov.companieshouse.api.model.ApiResponse;
 import uk.gov.companieshouse.api.sdk.ApiClientService;
+import uk.gov.companieshouse.pscdataapi.exceptions.ServiceUnavailableException;
 import uk.gov.companieshouse.pscdataapi.util.TestHelper;
 
 import static org.mockito.Mockito.verifyNoInteractions;
-import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @ActiveProfiles("feature_flag_enabled")
 class ResourceChangedApiServiceAspectFeatureFlagEnabledITest {
-//    @Autowired
-//    private ResourceChangedApiService resourceChangedApiService;
-//
-//    @MockBean
-//    private ApiClientService apiClientService;
-//
-//    @Mock
-//    private InternalApiClient internalApiClient;
-//    @Mock
-//    private ResourceChangedRequest resourceChangedRequest;
-//    @Mock
-//    private PrivateChangedResourceHandler privateChangedResourceHandler;
-//    @Mock
-//    private PrivateChangedResourcePost changedResourcePost;
-//
-//    @Test
-//    void testThatAspectShouldNotProceedWhenFeatureFlagEnabled() throws ServiceUnavailableException {
-//
-//        resourceChangedApiService.invokeChsKafkaApi(resourceChangedRequest);
-//
-//        verifyNoInteractions(apiClientService);
-//        verifyNoInteractions(internalApiClient);
-//        verifyNoInteractions(privateChangedResourceHandler);
-//        verifyNoInteractions(changedResourcePost);
-//    }
+    @InjectMocks
+    private ChsKafkaApiService chsKafkaApiService;
+
+    @MockBean
+    private ApiClientService apiClientService;
+
+    @Mock
+    private InternalApiClient internalApiClient;
+    @Mock
+    private PrivateChangedResourceHandler privateChangedResourceHandler;
+    @Mock
+    private PrivateChangedResourcePost changedResourcePost;
+
+    private TestHelper testHelper;
+
+    @BeforeEach
+    void setup() {
+        testHelper = new TestHelper();
+    }
+    @Test
+    void testThatAspectShouldNotProceedWhenFeatureFlagEnabled() throws ServiceUnavailableException {
+
+        chsKafkaApiService.invokeChsKafkaApi(testHelper.X_REQUEST_ID, testHelper.COMPANY_NUMBER, testHelper.NOTIFICATION_ID, "kind");
+
+        //verifyNoInteractions(apiClientService);
+        verifyNoInteractions(internalApiClient);
+        verifyNoInteractions(privateChangedResourceHandler);
+        verifyNoInteractions(changedResourcePost);
+    }
 }
