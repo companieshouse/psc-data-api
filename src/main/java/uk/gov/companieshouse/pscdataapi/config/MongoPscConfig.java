13a3b62e5a21779f79e135dc029168742612f283
JAndrewCH <jandrew@companieshouse.gov.uk>
2024-03-08 11:49:41 +0000

    add custom conversions in mongo psc config

--- src/main/java/uk/gov/companieshouse/pscdataapi/config/MongoPscConfig.java
+++ src/main/java/uk/gov/companieshouse/pscdataapi/config/MongoPscConfig.java
@@ -4,12 +4,14 @@ import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.MongoTransactionManager;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
@@ -22,9 +24,17 @@ public class MongoPscConfig extends AbstractMongoClientConfiguration {
     @Value("${spring.data.mongodb.uri}")
     private String databaseUri;
 
+    @Autowired
+    MongoCustomConversions mongoCustomConversions;
+
     @Bean
     MongoTransactionManager transactionManager(MongoDatabaseFactory dbFactory) {
         return new MongoTransactionManager(dbFactory);
+    }
+
+    @Override
+    public MongoCustomConversions customConversions() {
+        return this.mongoCustomConversions;
     }
 
     @Override
