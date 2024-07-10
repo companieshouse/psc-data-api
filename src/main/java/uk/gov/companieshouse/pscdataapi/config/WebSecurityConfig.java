b337c22c2c860cf721cdd49205edc400339cf86a
dtoulcher <dtoulcher@companieshouse.gov.uk>
2024-07-10 09:16:08 +0100

    feat: added required configuration to allow tests to pass
    SEC-56

--- src/main/java/uk/gov/companieshouse/pscdataapi/config/WebSecurityConfig.java
+++ src/main/java/uk/gov/companieshouse/pscdataapi/config/WebSecurityConfig.java
@@ -4,15 +4,22 @@ import java.util.Arrays;
 import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import uk.gov.companieshouse.api.filter.CustomCorsFilter;
 import uk.gov.companieshouse.api.interceptor.InternalUserInterceptor;
 import uk.gov.companieshouse.api.interceptor.UserAuthenticationInterceptor;
 
 @Configuration
 public class WebSecurityConfig implements WebMvcConfigurer {
 
-    List<String> externalMethods = Arrays.asList("GET");
+    List<String> externalMethods = Arrays.asList(HttpMethod.GET.name());
     List<String> otherAllowedAuthMethods = Arrays.asList("oauth2");
 
     @Override
@@ -33,5 +40,19 @@ public class WebSecurityConfig implements WebMvcConfigurer {
         return new UserAuthenticationInterceptor(externalMethods, 
                                                  otherAllowedAuthMethods, 
                                                  internalUserInterceptor());
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.cors(AbstractHttpConfigurer::disable)
+            .csrf(AbstractHttpConfigurer::disable)
+            .addFilterBefore(new CustomCorsFilter(externalMethods()), CsrfFilter.class);
+
+        return http.build();
+    }
+
+    @Bean
+    public List<String> externalMethods() {
+        return Arrays.asList(HttpMethod.GET.name());
     }
 }
