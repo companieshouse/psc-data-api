95ce0aa2fd5f78a8d000ec9d6f87341974e3701c
cgregoryCH <cgregory@companieshouse.gov.uk>
2024-07-18 11:09:31 +0100

    Fix ECS routing priority so psc-statement-data-api routes are hit first

--- terraform/groups/ecs-service/locals.tf
+++ terraform/groups/ecs-service/locals.tf
@@ -8,7 +8,7 @@ locals {
   eric_port                   = "10000"
   docker_repo                 = "psc-data-api"
   kms_alias                   = "alias/${var.aws_profile}/environment-services-kms"
-  lb_listener_rule_priority   = 84
+  lb_listener_rule_priority   = 88
   lb_listener_paths           = [
     "/psc-data-api/healthcheck", "/company/*/persons-with-significant-control*"
   ]
