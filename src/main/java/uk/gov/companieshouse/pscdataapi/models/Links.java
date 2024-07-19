7856022145b7ed5084dc2efaa54394f8ded987a3
SALMANCH007 <salhadad@companieshouse.gov.uk>
2024-07-19 15:42:59 +0100

    added exemptions logic

--- src/main/java/uk/gov/companieshouse/pscdataapi/models/Links.java
+++ src/main/java/uk/gov/companieshouse/pscdataapi/models/Links.java
@@ -11,6 +11,9 @@ public class Links {
     @Field("statement")
     private String statement;
 
+    @Field("exemptions")
+    private String exemptions;
+
     public String getSelf() {
         return self;
     }
@@ -27,6 +30,10 @@ public class Links {
         this.statement = statement;
     }
 
+    public String getExemptions() { return exemptions; }
+
+    public void setExemptions(String exemptions) { this.exemptions = exemptions; }
+
     @Override
     public String toString() {
         return "Links{"
@@ -35,6 +42,9 @@ public class Links {
                 + '\''
                 + ", statement='"
                 + statement
+                + '\''
+                + ", exemptions='"
+                + exemptions
                 + '\''
                 + '}';
     }
@@ -49,11 +59,12 @@ public class Links {
         }
         Links links = (Links) object;
         return Objects.equals(self, links.self)
-                && Objects.equals(statement, links.statement);
+                && Objects.equals(statement, links.statement)
+                && Objects.equals(exemptions, links.exemptions);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(self, statement);
+        return Objects.hash(self, statement, exemptions);
     }
 }
