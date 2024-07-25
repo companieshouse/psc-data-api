28319075db34a3b9226c238418c42828cbf4251c
SALMANCH007 <135017197+SALMANCH007@users.noreply.github.com>
2024-07-25 13:19:52 +0100

    Revert "Feature/dsnd 2789"

--- src/test/java/uk/gov/companieshouse/pscdataapi/PscDataApiApplicationTests.java
+++ src/test/java/uk/gov/companieshouse/pscdataapi/PscDataApiApplicationTests.java
@@ -2,13 +2,9 @@ package uk.gov.companieshouse.pscdataapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import uk.gov.companieshouse.api.api.CompanyExemptionsApiService;
 
 @SpringBootTest
 class PscDataApiApplicationTests {
-    @MockBean
-    CompanyExemptionsApiService companyExemptionsApiService;
 
     @Test
     void contextLoads() {
