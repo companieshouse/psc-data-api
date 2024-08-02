dcf259d4e9d02e9910af166741f630164d265348
maddytjCH <mtjalma@companieshouse.gov.uk>
2024-08-02 15:50:19 +0100

    add additional unit tests

--- src/test/java/uk/gov/companieshouse/pscdataapi/api/ChsKafkaApiServiceTest.java
+++ src/test/java/uk/gov/companieshouse/pscdataapi/api/ChsKafkaApiServiceTest.java
@@ -25,8 +25,7 @@ import uk.gov.companieshouse.api.error.ApiErrorResponseException;
 import uk.gov.companieshouse.api.handler.chskafka.PrivateChangedResourceHandler;
 import uk.gov.companieshouse.api.handler.chskafka.request.PrivateChangedResourcePost;
 import uk.gov.companieshouse.api.model.ApiResponse;
-import uk.gov.companieshouse.api.psc.Individual;
-import uk.gov.companieshouse.api.psc.IndividualBeneficialOwner;
+import uk.gov.companieshouse.api.psc.*;
 import uk.gov.companieshouse.logging.Logger;
 import uk.gov.companieshouse.pscdataapi.exceptions.ServiceUnavailableException;
 import uk.gov.companieshouse.pscdataapi.models.PscDocument;
@@ -136,6 +135,189 @@ class ChsKafkaApiServiceTest {
     }
 
     @Test
+    void invokeChsKafkaEndpointWithDeleteForLegalPerson() throws ApiErrorResponseException, JsonProcessingException {
+        // given
+        when(internalApiClient.privateChangedResourceHandler()).thenReturn(privateChangedResourceHandler);
+        when(privateChangedResourceHandler.postChangedResource(any(), any())).thenReturn(privateChangedResourcePost);
+        when(privateChangedResourcePost.execute()).thenReturn(response);
+
+        PscDocument document = TestHelper.buildPscDocument(TestHelper.LEGAL_KIND);
+        LegalPerson legalPerson = new LegalPerson();
+        legalPerson.setKind(LegalPerson.KindEnum.LEGAL_PERSON_PERSON_WITH_SIGNIFICANT_CONTROL);
+        legalPerson.setName("Legal-Person");
+        when(companyPscTransformer.transformPscDocToLegalPerson(document)).thenReturn(legalPerson);
+
+        when(objectMapper.writeValueAsString(legalPerson)).thenReturn(legalPerson.toString());
+        when(objectMapper.readValue(legalPerson.toString(), Object.class)).thenReturn(legalPerson);
+
+        // when
+        ApiResponse<?> apiResponse = chsKafkaApiService.invokeChsKafkaApiWithDeleteEvent(
+                TestHelper.X_REQUEST_ID, TestHelper.COMPANY_NUMBER, TestHelper.NOTIFICATION_ID, TestHelper.LEGAL_KIND,
+                document);
+        Assertions.assertThat(apiResponse).isNotNull();
+
+        //then
+        verify(internalApiClient, times(1)).privateChangedResourceHandler();
+        verify(privateChangedResourceHandler, times(1)).postChangedResource(any(), changedResourceCaptor.capture());
+        verify(privateChangedResourcePost, times(1)).execute();
+        Assertions.assertThat(changedResourceCaptor.getValue().getEvent().getType()).isEqualTo(EVENT_TYPE_DELETED);
+        Assertions.assertThat(changedResourceCaptor.getValue().getDeletedData()).isInstanceOf(LegalPerson.class);
+    }
+
+    @Test
+    void invokeChsKafkaEndpointWithDeleteForLegalPersonBeneficialOwner() throws ApiErrorResponseException, JsonProcessingException {
+        // given
+        when(internalApiClient.privateChangedResourceHandler()).thenReturn(privateChangedResourceHandler);
+        when(privateChangedResourceHandler.postChangedResource(any(), any())).thenReturn(privateChangedResourcePost);
+        when(privateChangedResourcePost.execute()).thenReturn(response);
+
+        PscDocument document = TestHelper.buildPscDocument(TestHelper.LEGAL_BO_KIND);
+        LegalPersonBeneficialOwner legalPersonBeneficialOwner = new LegalPersonBeneficialOwner();
+        legalPersonBeneficialOwner.setKind(LegalPersonBeneficialOwner.KindEnum.LEGAL_PERSON_BENEFICIAL_OWNER);
+        legalPersonBeneficialOwner.setName("Legal-Person-Beneficial-Owner");
+        when(companyPscTransformer.transformPscDocToLegalPersonBeneficialOwner(
+                document)).thenReturn(legalPersonBeneficialOwner);
+
+        when(objectMapper.writeValueAsString(legalPersonBeneficialOwner))
+                .thenReturn(legalPersonBeneficialOwner.toString());
+        when(objectMapper.readValue(legalPersonBeneficialOwner.toString(), Object.class))
+                .thenReturn(legalPersonBeneficialOwner);
+
+        // when
+        ApiResponse<?> apiResponse = chsKafkaApiService.invokeChsKafkaApiWithDeleteEvent(
+                TestHelper.X_REQUEST_ID, TestHelper.COMPANY_NUMBER, TestHelper.NOTIFICATION_ID, TestHelper.LEGAL_BO_KIND,
+                document);
+        Assertions.assertThat(apiResponse).isNotNull();
+
+        //then
+        verify(internalApiClient, times(1)).privateChangedResourceHandler();
+        verify(privateChangedResourceHandler, times(1)).postChangedResource(any(), changedResourceCaptor.capture());
+        verify(privateChangedResourcePost, times(1)).execute();
+        Assertions.assertThat(changedResourceCaptor.getValue().getEvent().getType()).isEqualTo(EVENT_TYPE_DELETED);
+        Assertions.assertThat(changedResourceCaptor.getValue().getDeletedData()).isInstanceOf(LegalPersonBeneficialOwner.class);
+    }
+
+    @Test
+    void invokeChsKafkaEndpointWithDeleteForSuperSecure() throws ApiErrorResponseException, JsonProcessingException {
+        // given
+        when(internalApiClient.privateChangedResourceHandler()).thenReturn(privateChangedResourceHandler);
+        when(privateChangedResourceHandler.postChangedResource(any(), any())).thenReturn(privateChangedResourcePost);
+        when(privateChangedResourcePost.execute()).thenReturn(response);
+
+        PscDocument document = TestHelper.buildPscDocument(TestHelper.SECURE_KIND);
+        SuperSecure superSecure = new SuperSecure();
+        superSecure.setKind(SuperSecure.KindEnum.SUPER_SECURE_PERSON_WITH_SIGNIFICANT_CONTROL);
+        when(companyPscTransformer.transformPscDocToSuperSecure(
+                document)).thenReturn(superSecure);
+
+        when(objectMapper.writeValueAsString(superSecure)).thenReturn(superSecure.toString());
+        when(objectMapper.readValue(superSecure.toString(), Object.class)).thenReturn(superSecure);
+
+        // when
+        ApiResponse<?> apiResponse = chsKafkaApiService.invokeChsKafkaApiWithDeleteEvent(
+                TestHelper.X_REQUEST_ID, TestHelper.COMPANY_NUMBER, TestHelper.NOTIFICATION_ID, TestHelper.SECURE_KIND,
+                document);
+        Assertions.assertThat(apiResponse).isNotNull();
+
+        //then
+        verify(internalApiClient, times(1)).privateChangedResourceHandler();
+        verify(privateChangedResourceHandler, times(1)).postChangedResource(any(), changedResourceCaptor.capture());
+        verify(privateChangedResourcePost, times(1)).execute();
+        Assertions.assertThat(changedResourceCaptor.getValue().getEvent().getType()).isEqualTo(EVENT_TYPE_DELETED);
+        Assertions.assertThat(changedResourceCaptor.getValue().getDeletedData()).isInstanceOf(SuperSecure.class);
+    }
+
+    @Test
+    void invokeChsKafkaEndpointWithDeleteForSuperSecureBO() throws ApiErrorResponseException, JsonProcessingException {
+        // given
+        when(internalApiClient.privateChangedResourceHandler()).thenReturn(privateChangedResourceHandler);
+        when(privateChangedResourceHandler.postChangedResource(any(), any())).thenReturn(privateChangedResourcePost);
+        when(privateChangedResourcePost.execute()).thenReturn(response);
+
+        PscDocument document = TestHelper.buildPscDocument(TestHelper.SECURE_BO_KIND);
+        SuperSecureBeneficialOwner superSecureBO = new SuperSecureBeneficialOwner();
+        superSecureBO.setKind(SuperSecureBeneficialOwner.KindEnum.SUPER_SECURE_BENEFICIAL_OWNER);
+        when(companyPscTransformer.transformPscDocToSuperSecureBeneficialOwner(
+                document)).thenReturn(superSecureBO);
+
+        when(objectMapper.writeValueAsString(superSecureBO)).thenReturn(superSecureBO.toString());
+        when(objectMapper.readValue(superSecureBO.toString(), Object.class)).thenReturn(superSecureBO);
+
+        // when
+        ApiResponse<?> apiResponse = chsKafkaApiService.invokeChsKafkaApiWithDeleteEvent(
+                TestHelper.X_REQUEST_ID, TestHelper.COMPANY_NUMBER, TestHelper.NOTIFICATION_ID, TestHelper.SECURE_BO_KIND,
+                document);
+        Assertions.assertThat(apiResponse).isNotNull();
+
+        //then
+        verify(internalApiClient, times(1)).privateChangedResourceHandler();
+        verify(privateChangedResourceHandler, times(1)).postChangedResource(any(), changedResourceCaptor.capture());
+        verify(privateChangedResourcePost, times(1)).execute();
+        Assertions.assertThat(changedResourceCaptor.getValue().getEvent().getType()).isEqualTo(EVENT_TYPE_DELETED);
+        Assertions.assertThat(changedResourceCaptor.getValue().getDeletedData()).isInstanceOf(SuperSecureBeneficialOwner.class);
+    }
+
+    @Test
+    void invokeChsKafkaEndpointWithDeleteForCorporateEntity() throws ApiErrorResponseException, JsonProcessingException {
+        // given
+        when(internalApiClient.privateChangedResourceHandler()).thenReturn(privateChangedResourceHandler);
+        when(privateChangedResourceHandler.postChangedResource(any(), any())).thenReturn(privateChangedResourcePost);
+        when(privateChangedResourcePost.execute()).thenReturn(response);
+
+        PscDocument document = TestHelper.buildPscDocument(TestHelper.CORPORATE_KIND);
+        CorporateEntity corporateEntity = new CorporateEntity();
+        corporateEntity.setKind(CorporateEntity.KindEnum.CORPORATE_ENTITY_PERSON_WITH_SIGNIFICANT_CONTROL);
+        corporateEntity.setName("Corporate-Entity-Person-With-Significant-Control");
+        when(companyPscTransformer.transformPscDocToCorporateEntity(document)).thenReturn(corporateEntity);
+
+        when(objectMapper.writeValueAsString(corporateEntity)).thenReturn(corporateEntity.toString());
+        when(objectMapper.readValue(corporateEntity.toString(), Object.class)).thenReturn(corporateEntity);
+
+        // when
+        ApiResponse<?> apiResponse = chsKafkaApiService.invokeChsKafkaApiWithDeleteEvent(
+                TestHelper.X_REQUEST_ID, TestHelper.COMPANY_NUMBER, TestHelper.NOTIFICATION_ID, TestHelper.CORPORATE_KIND,
+                document);
+        Assertions.assertThat(apiResponse).isNotNull();
+
+        //then
+        verify(internalApiClient, times(1)).privateChangedResourceHandler();
+        verify(privateChangedResourceHandler, times(1)).postChangedResource(any(), changedResourceCaptor.capture());
+        verify(privateChangedResourcePost, times(1)).execute();
+        Assertions.assertThat(changedResourceCaptor.getValue().getEvent().getType()).isEqualTo(EVENT_TYPE_DELETED);
+        Assertions.assertThat(changedResourceCaptor.getValue().getDeletedData()).isInstanceOf(CorporateEntity.class);
+    }
+
+    @Test
+    void invokeChsKafkaEndpointWithDeleteForCorporateEntityBO() throws ApiErrorResponseException, JsonProcessingException {
+        // given
+        when(internalApiClient.privateChangedResourceHandler()).thenReturn(privateChangedResourceHandler);
+        when(privateChangedResourceHandler.postChangedResource(any(), any())).thenReturn(privateChangedResourcePost);
+        when(privateChangedResourcePost.execute()).thenReturn(response);
+
+        PscDocument document = TestHelper.buildPscDocument(TestHelper.CORPORATE_BO_KIND);
+        CorporateEntityBeneficialOwner corporateEntityBO = new CorporateEntityBeneficialOwner();
+        corporateEntityBO.setKind(CorporateEntityBeneficialOwner.KindEnum.CORPORATE_ENTITY_BENEFICIAL_OWNER);
+        corporateEntityBO.setName("Corporate-Entity-Beneficial-Owner");
+        when(companyPscTransformer.transformPscDocToCorporateEntityBeneficialOwner(document)).thenReturn(corporateEntityBO);
+
+        when(objectMapper.writeValueAsString(corporateEntityBO)).thenReturn(corporateEntityBO.toString());
+        when(objectMapper.readValue(corporateEntityBO.toString(), Object.class)).thenReturn(corporateEntityBO);
+
+        // when
+        ApiResponse<?> apiResponse = chsKafkaApiService.invokeChsKafkaApiWithDeleteEvent(
+                TestHelper.X_REQUEST_ID, TestHelper.COMPANY_NUMBER, TestHelper.NOTIFICATION_ID, TestHelper.CORPORATE_BO_KIND,
+                document);
+        Assertions.assertThat(apiResponse).isNotNull();
+
+        //then
+        verify(internalApiClient, times(1)).privateChangedResourceHandler();
+        verify(privateChangedResourceHandler, times(1)).postChangedResource(any(), changedResourceCaptor.capture());
+        verify(privateChangedResourcePost, times(1)).execute();
+        Assertions.assertThat(changedResourceCaptor.getValue().getEvent().getType()).isEqualTo(EVENT_TYPE_DELETED);
+        Assertions.assertThat(changedResourceCaptor.getValue().getDeletedData()).isInstanceOf(CorporateEntityBeneficialOwner.class);
+    }
+
+    @Test
     void invokeChsKafkaEndpointThrowsApiErrorException() throws ApiErrorResponseException {
         ApiErrorResponseException exception = new ApiErrorResponseException(new HttpResponseException.Builder(408, "Test Request timeout", new HttpHeaders()));
         when(internalApiClient.privateChangedResourceHandler()).thenReturn(privateChangedResourceHandler);
@@ -201,19 +383,5 @@ class ChsKafkaApiServiceTest {
         verify(privateChangedResourceHandler, times(1)).postChangedResource(any(), changedResourceCaptor.capture());
         verify(privateChangedResourcePost, times(1)).execute();
         Assertions.assertThat(changedResourceCaptor.getValue().getEvent().getType()).isEqualTo(EVENT_TYPE_DELETED);
-    }
-
-    @Test
-    void invokeChsKafkaEndpointDeleteReturnsCorrectDocument() throws ApiErrorResponseException {
-        when(internalApiClient.privateChangedResourceHandler()).thenReturn(privateChangedResourceHandler);
-        when(privateChangedResourceHandler.postChangedResource(any(), any())).thenReturn(privateChangedResourcePost);
-        when(privateChangedResourcePost.execute()).thenReturn(response);
-
-        ApiResponse<?> apiResponse = chsKafkaApiService.invokeChsKafkaApiWithDeleteEvent(
-                TestHelper.X_REQUEST_ID, TestHelper.COMPANY_NUMBER, TestHelper.NOTIFICATION_ID, "kind",
-                TestHelper.buildPscDocument("individual-persons-with-significant-control"));
-        Assertions.assertThat(apiResponse).isNotNull();
-
-        Assertions.assertThat(changedResourceCaptor.getValue().getDeletedData()).isInstanceOf(Individual.class);
     }
 }
