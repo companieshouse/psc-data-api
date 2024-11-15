78990957b289b284f0edfb1fee128c3211576a26
PhilipGlover <pglover@companieshouse.gov.uk>
2024-11-15 13:55:41 +0000

    add data to check in test

--- src/test/java/uk/gov/companieshouse/pscdataapi/controller/CompanyPscFullRecordGetControllerTest.java
+++ src/test/java/uk/gov/companieshouse/pscdataapi/controller/CompanyPscFullRecordGetControllerTest.java
@@ -9,8 +9,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.time.Instant;
-import java.time.OffsetDateTime;
-import java.time.ZoneId;
+import java.util.Arrays;
+
 import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -19,10 +19,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
-import uk.gov.companieshouse.api.psc.ExternalData;
-import uk.gov.companieshouse.api.psc.FullRecordCompanyPSCApi;
-import uk.gov.companieshouse.api.psc.IndividualFullRecord;
-import uk.gov.companieshouse.api.psc.InternalData;
+import uk.gov.companieshouse.api.psc.*;
 import uk.gov.companieshouse.pscdataapi.service.CompanyPscService;
 
 @SpringBootTest(properties = {"feature.psc_individual_full_record_get=true"})
@@ -65,14 +62,40 @@ class CompanyPscFullRecordGetControllerTest {
             .contentType(APPLICATION_JSON)
             .header("x-request-id", X_REQUEST_ID)
             .header("ERIC-Authorised-Key-Roles", ERIC_PRIVILEGES)
-                .header("ERIC-Authorised-Key-Privileges", ERIC_AUTH_INTERNAL)).andExpect(status().isOk())
+            .header("ERIC-Authorised-Key-Privileges", ERIC_AUTH_INTERNAL)).andExpect(status().isOk())
             .andDo(print())
-            .andExpect(jsonPath("$.kind").value("individual-person-with-significant-control"));
-        // TODO: expect more (dummy) data in response
+            .andExpect(jsonPath("$.kind").value("individual-person-with-significant-control"))
+            .andExpect(jsonPath("$.name").value("Andy Bob Smith"))
+            .andExpect(jsonPath("$.name_elements.forename").value("Andy"))
+            .andExpect(jsonPath("$.name_elements.middle_name").value("Bob"))
+            .andExpect(jsonPath("$.name_elements.surname").value("Smith"))
+            .andExpect(jsonPath("$.address.address_line_1").value("addressLine1"))
+            .andExpect(jsonPath("$.address.postal_code").value("CF12 3AB"))
+            .andExpect(jsonPath("$.address.premises").value("1"))
+            .andExpect(jsonPath("$.residential_address_same_as_service_address").value(Boolean.FALSE))
+            .andExpect(jsonPath("$.usual_residential_address.address_line_1").value("Home street"))
+            .andExpect(jsonPath("$.usual_residential_address.postal_code").value("AB12 3CD"))
+            .andExpect(jsonPath("$.usual_residential_address.premise").value("Cottage"))
+            .andExpect(jsonPath("$.nationality").value("British"))
+            .andExpect(jsonPath("$.natures_of_control[0]").value("nature of my control"))
+            .andExpect(jsonPath("$.date_of_birth.day").value(1))
+            .andExpect(jsonPath("$.date_of_birth.month").value(2))
+            .andExpect(jsonPath("$.date_of_birth.year").value(2000))
+            .andExpect(jsonPath("$.links[0].self").value("/company/123/persons-with-significant-control/456")) ;
     }
 
     private static @NotNull IndividualFullRecord createFullRecord() {
-        return new IndividualFullRecord().kind(IndividualFullRecord.KindEnum.INDIVIDUAL_PERSON_WITH_SIGNIFICANT_CONTROL);
+        return new IndividualFullRecord()
+                .kind(IndividualFullRecord.KindEnum.INDIVIDUAL_PERSON_WITH_SIGNIFICANT_CONTROL)
+                .name("Andy Bob Smith")
+                .nameElements(new NameElements().forename("Andy").middleName("Bob").surname("Smith"))
+                .address(new Address("addressLine1", "CF12 3AB", "1"))
+                .residentialAddressSameAsServiceAddress(Boolean.FALSE)
+                .usualResidentialAddress(new UsualResidentialAddress().premise("Cottage").addressLine1("Home street").postalCode("AB12 3CD"))
+                .nationality("British")
+                .naturesOfControl(Arrays.asList("nature of my control"))
+                .dateOfBirth(new DateOfBirth().day(1).month(2).year(2000))
+                .links(Arrays.asList(new ItemLinkTypes().self("/company/123/persons-with-significant-control/456")));
     }
 
 }
