6c47caaa98c693a8e0752e38af062bc1af580fa9
sthompsonCH <sthompson@companieshouse.gov.uk>
2025-01-13 16:20:24 +0000

    DSND-3213: Get tests passing after removing env vars from pom

--- src/main/java/uk/gov/companieshouse/pscdataapi/config/ApplicationConfig.java
+++ src/main/java/uk/gov/companieshouse/pscdataapi/config/ApplicationConfig.java
@@ -16,12 +16,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
 import uk.gov.companieshouse.api.InternalApiClient;
+import uk.gov.companieshouse.api.converter.EnumWriteConverter;
 import uk.gov.companieshouse.api.http.ApiKeyHttpClient;
 import uk.gov.companieshouse.pscdataapi.converter.CompanyPscReadConverter;
 import uk.gov.companieshouse.pscdataapi.converter.CompanyPscSensitiveReadConverter;
 import uk.gov.companieshouse.pscdataapi.converter.CompanyPscSensitiveWriteConverter;
 import uk.gov.companieshouse.pscdataapi.converter.CompanyPscWriteConverter;
-import uk.gov.companieshouse.pscdataapi.converter.EnumWriteConverter;
 import uk.gov.companieshouse.pscdataapi.logging.DataMapHolder;
 import uk.gov.companieshouse.pscdataapi.models.PscData;
 import uk.gov.companieshouse.pscdataapi.models.PscSensitiveData;
@@ -32,24 +32,24 @@ import uk.gov.companieshouse.pscdataapi.serialization.LocalDateSerializer;
 public class ApplicationConfig {
 
     private final String apiKey;
-    private final String kafkaApiUrl;
+    private final String internalApiUrl;
     private final String metricsApiUrl;
     private final String exemptionsApiUrl;
 
     public ApplicationConfig(@Value("${api.key}") String apiKey,
-            @Value("${kafka.api.url}") String kafkaApiUrl,
+            @Value("${kafka.api.url}") String internalApiUrl,
             @Value("${metrics.api.url}") String metricsApiUrl,
             @Value("${exemptions.api.url}") String exemptionsApiUrl) {
         this.apiKey = apiKey;
-        this.kafkaApiUrl = kafkaApiUrl;
+        this.internalApiUrl = internalApiUrl;
         this.metricsApiUrl = metricsApiUrl;
         this.exemptionsApiUrl = exemptionsApiUrl;
     }
 
-    /*
-    These custom converters allow us to only use @JsonProperty instead of needing the @Field annotation also on fields
-    that require snake casing. Without these custom converters, and in the absence of the @Field annotation, we would
-    get camel casing on fields with multiple words.
+    /**
+     * mongoCustomConversions.
+     *
+     * @return MongoCustomConversions.
      */
     @Bean
     public MongoCustomConversions mongoCustomConversions() {
@@ -63,7 +63,7 @@ public class ApplicationConfig {
 
     @Bean
     public Supplier<InternalApiClient> kafkaApiClientSupplier() {
-        return () -> buildClient(kafkaApiUrl);
+        return () -> buildClient(internalApiUrl);
     }
 
     @Bean
@@ -76,6 +76,21 @@ public class ApplicationConfig {
         return () -> buildClient(exemptionsApiUrl);
     }
 
+//    @Bean
+//    public InternalApiClient internalApiClient() {
+//        return ApiSdkManager.getPrivateSDK();
+//    }
+
+//    @Bean
+//    public CompanyMetricsApiService companyMetricsApiService() {
+//        return new CompanyMetricsApiService();
+//    }
+//
+//    @Bean
+//    public CompanyExemptionsApiService companyExemptionsApiService(){
+//        return new CompanyExemptionsApiService();
+//    }
+
     @Bean
     @Primary
     public ObjectMapper objectMapper() {
@@ -87,6 +102,11 @@ public class ApplicationConfig {
                 .configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
     }
 
+    /**
+     * Mongo DB Object Mapper.
+     *
+     * @return ObjectMapper.
+     */
     private ObjectMapper mongoDbObjectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
