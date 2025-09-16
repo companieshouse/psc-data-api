d6dc9f75de3c180a2799dcc64e1c63571b78a11c
PhilipGlover <pglover@companieshouse.gov.uk>
2025-09-16 13:58:53 +0100

    remove feature flag that is no longer used

--- src/main/java/uk/gov/companieshouse/pscdataapi/config/FeatureFlags.java
+++ src/main/java/uk/gov/companieshouse/pscdataapi/config/FeatureFlags.java
@@ -8,14 +8,12 @@ public class FeatureFlags {
 
     private final boolean streamHookDisabled;
     private final boolean identityVerificationEnabled;
-    private final boolean individualPscFullRecordAddidentityVerificationDetailsEnabled;
 
     public FeatureFlags(@Value("${feature.seeding_collection_enabled}") final boolean streamHookDisabled,
             @Value("${feature.identity_verification}") final boolean identityVerificationEnabled,
         @Value("${feature.psc_individual_full_record_add_identity_verification_details}") final boolean individualPscFullRecordAddidentityVerificationDetailsEnabled) {
         this.streamHookDisabled = streamHookDisabled;
         this.identityVerificationEnabled = identityVerificationEnabled;
-        this.individualPscFullRecordAddidentityVerificationDetailsEnabled = individualPscFullRecordAddidentityVerificationDetailsEnabled;
     }
 
     public boolean isStreamHookDisabled() {
@@ -26,7 +24,4 @@ public class FeatureFlags {
         return identityVerificationEnabled;
     }
 
-    public boolean isIndividualPscFullRecordAddidentityVerificationDetailsEnabled() {
-        return individualPscFullRecordAddidentityVerificationDetailsEnabled;
-    }
 }
