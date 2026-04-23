d291e855eefc17b1e40531b51696a4f92b3deca3
brdywltn <bwilton@companieshouse.gov.uk>
2026-04-23 14:15:44 +0100

    Update skeleton for PscNotificationsController

--- src/main/java/uk/gov/companieshouse/pscdataapi/pscnotifications/PscNotificationsController.java
+++ src/main/java/uk/gov/companieshouse/pscdataapi/pscnotifications/PscNotificationsController.java
@@ -1,4 +1,25 @@
 package uk.gov.companieshouse.pscdataapi.pscnotifications;
 
+import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import uk.gov.companieshouse.logging.Logger;
+import uk.gov.companieshouse.logging.LoggerFactory;
+import uk.gov.companieshouse.pscdataapi.PscDataApiApplication;
+
+@Controller
 public class PscNotificationsController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PscDataApiApplication.APPLICATION_NAME_SPACE);
+
+    private final PscNotificationsService service;
+
+    PscNotificationsController(PscNotificationsService service) {
+        this.service = service;
+    }
+
+    @GetMapping(path = "/persons-with-significant-control/{psc_id}/notifications")
+    public ResponseEntity<NotificationList> getPscNotifications(){
+
+    }
 }
