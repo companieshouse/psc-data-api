48bfdc6c3fd0a70f5e5fd6bf373253e7e5c13ab7
brdywltn <bwilton@companieshouse.gov.uk>
2026-04-28 12:15:51 +0100

    Fill in skeleton for PscNotificationsController

--- src/main/java/uk/gov/companieshouse/pscdataapi/pscnotifications/PscNotificationsController.java
+++ src/main/java/uk/gov/companieshouse/pscdataapi/pscnotifications/PscNotificationsController.java
@@ -1,11 +1,18 @@
 package uk.gov.companieshouse.pscdataapi.pscnotifications;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import uk.gov.companieshouse.logging.Logger;
 import uk.gov.companieshouse.logging.LoggerFactory;
 import uk.gov.companieshouse.pscdataapi.PscDataApiApplication;
+import uk.gov.companieshouse.pscdataapi.exceptions.BadRequestException;
+import uk.gov.companieshouse.pscdataapi.logging.DataMapHolder;
+
+import static uk.gov.companieshouse.pscdataapi.interceptor.AuthenticationHelperImpl.ERIC_AUTHORISED_KEY_PRIVILEGES_HEADER;
 
 @Controller
 public class PscNotificationsController {
@@ -19,7 +26,34 @@ public class PscNotificationsController {
     }
 
     @GetMapping(path = "/persons-with-significant-control/{psc_id}/notifications")
-    public ResponseEntity<NotificationList> getPscNotifications(){
+    public ResponseEntity<NotificationList> getPscNotifications(
+            @PathVariable("psc_id") String pscId,
+            @RequestParam(value = "filter", required = false) String filter,
+            @RequestParam(value = "start_index", required = false) Integer startIndex,
+            @RequestParam(value = "items_per_page", required = false) Integer itemsPerPage,
+            @RequestParam(value = ERIC_AUTHORISED_KEY_PRIVILEGES_HEADER, required = false) String authPrivileges) {
+        try {
+            DataMapHolder.get().pscId(pscId);
+            LOGGER.info("Fetching psc notifications", DataMapHolder.getLogMap());
 
+            PscNotificationsRequest request = PscNotificationsRequest.builder()
+                    .pscId(pscId)
+                    .filter(filter)
+                    .startIndex(startIndex)
+                    .itemsPerPage(itemsPerPage)
+                    .authPrivileges(authPrivileges)
+                    .build();
+            return service.getPscNotifications(request)
+                    .map(ResponseEntity::ok)
+                    .orElseGet(() -> {
+                        LOGGER.info(String.format("No notifications found for psc ID: %s", pscId),
+                                DataMapHolder.getLogMap());
+                        return ResponseEntity.notFound().build();
+                    });
+        } catch (BadRequestException ex) {
+            LOGGER.info(String.format("Invalid filter parameter supplied: %s, psc ID: %s", filter, pscId),
+                    DataMapHolder.getLogMap());
+            return ResponseEntity.badRequest().build();
+        }
     }
 }
