7edc266ad8c72baa5c01b8d22d44c51370f2080a
brdywltn <bwilton@companieshouse.gov.uk>
2026-04-30 15:03:33 +0100

    Implement DateOfBirthMapper

--- src/main/java/uk/gov/companieshouse/pscdataapi/pscnotifications/mappers/DateOfBirthMapper.java
+++ src/main/java/uk/gov/companieshouse/pscdataapi/pscnotifications/mappers/DateOfBirthMapper.java
@@ -1,7 +1,24 @@
 package uk.gov.companieshouse.pscdataapi.pscnotifications.mappers;
 
 import org.springframework.stereotype.Component;
+import uk.gov.companieshouse.api.psc.DateOfBirth;
+
+import java.time.Instant;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.util.Optional;
+
+import static java.util.Optional.ofNullable;
 
 @Component
 public class DateOfBirthMapper {
+
+    Optional<DateOfBirth> map(Instant dateOfBirth){
+        return ofNullable(dateOfBirth)
+                .map(dob -> ZonedDateTime.ofInstant(dateOfBirth, ZoneOffset.UTC))
+                .map(zdt -> new DateOfBirth()
+                        .month(zdt.getMonthValue())
+                        .year(zdt.getYear()));
+
+    }
 }
