ffa5510adc6249637f34876f9dfa1bb46cc9a1ea
brdywltn <bwilton@companieshouse.gov.uk>
2026-04-30 09:53:19 +0100

    Add skeleton for PscNotificationDocument

---
+++ src/main/java/uk/gov/companieshouse/pscdataapi/models/PscNotificationDocument.java
@@ -0,0 +1,7 @@
+package uk.gov.companieshouse.pscdataapi.models;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "delta_notifications")
+public class PscNotificationDocument {
+}
