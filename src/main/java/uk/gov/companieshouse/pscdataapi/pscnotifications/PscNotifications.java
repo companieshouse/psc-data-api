b608541156bcb49a6f32dacc199d2a58e2c91778
brdywltn <bwilton@companieshouse.gov.uk>
2026-05-18 15:28:56 +0100

    Implement repository

--- src/main/java/uk/gov/companieshouse/pscdataapi/pscnotifications/PscNotifications.java
+++
@@ -1,4 +0,0 @@
-package uk.gov.companieshouse.pscdataapi.pscnotifications;
-
-public class PscNotifications {
-}
--- src/main/java/uk/gov/companieshouse/pscdataapi/pscnotifications/PscNotificationsRepository.java
+++ src/main/java/uk/gov/companieshouse/pscdataapi/pscnotifications/PscNotificationsRepository.java
@@ -1,11 +1,23 @@
 package uk.gov.companieshouse.pscdataapi.pscnotifications;
 
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
+import uk.gov.companieshouse.pscdataapi.models.PscDocument;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
-interface PscNotificationsRepository extends MongoRepository<PscNotificationDocument, String> {
+interface PscNotificationsRepository extends MongoRepository<PscDocument, String> {
 
-    List<PscNotificationDocument> findAll();
+    List<PscDocument> findAll();
 
-    PscNotificationDocument findById(String pscId);
+    Optional<PscDocument> findById(String pscId);
+
+    @Query(value = "{ $and: [ "
+            + "{'psc_id': ?0 },"
+            + "{ 'company_status': { $nin: ?2 } }"
+            + "] }", count = true)
+    int countTotal(String pscId);
 }
