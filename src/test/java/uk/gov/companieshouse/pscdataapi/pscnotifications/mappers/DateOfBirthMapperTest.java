6b03943a401a4c02ee27d8a3aba5a1207e3f816a
brdywltn <bwilton@companieshouse.gov.uk>
2026-05-21 15:48:33 +0100

    Implement tests for DateOfBirthMapper

--- src/test/java/uk/gov/companieshouse/pscdataapi/pscnotifications/mappers/DateOfBirthMapperTest.java
+++ src/test/java/uk/gov/companieshouse/pscdataapi/pscnotifications/mappers/DateOfBirthMapperTest.java
@@ -1,4 +1,31 @@
 package uk.gov.companieshouse.pscdataapi.pscnotifications.mappers;
 
-public class DateOfBirthMapperTest {
-}
+import org.junit.jupiter.api.Test;
+import uk.gov.companieshouse.api.psc_notifications.DateOfBirth;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+class DateOfBirthMapperTest {
+
+    private final DateOfBirthMapper mapper = new DateOfBirthMapper();
+
+    @Test
+    void testDateOfBirthMapper_mapsDateOfBirth_whenGivenValidInput() {
+        uk.gov.companieshouse.pscdataapi.models.DateOfBirth dob =
+                new uk.gov.companieshouse.pscdataapi.models.DateOfBirth();
+
+        dob.setMonth(10);
+        dob.setYear(2023);
+
+        DateOfBirth mappedDob = mapper.map(dob);
+
+        assertEquals(10, mappedDob.getMonth());
+        assertEquals(2023, mappedDob.getYear());
+    }
+
+    @Test
+    void testDateOfBirthMapper_returnsNull_whenProvidedNullDateOfBirth() {
+        assertNull(mapper.map(null));
+    }
+ }
