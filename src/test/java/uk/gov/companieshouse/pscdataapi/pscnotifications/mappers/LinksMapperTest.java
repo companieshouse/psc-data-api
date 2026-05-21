a4f38e0b4e1d0049d54cf57f744dc4fa8d17a3a6
brdywltn <bwilton@companieshouse.gov.uk>
2026-05-21 15:56:29 +0100

    Implement LinksMapper tests

--- src/test/java/uk/gov/companieshouse/pscdataapi/pscnotifications/mappers/LinksMapperTest.java
+++ src/test/java/uk/gov/companieshouse/pscdataapi/pscnotifications/mappers/LinksMapperTest.java
@@ -1,4 +1,28 @@
 package uk.gov.companieshouse.pscdataapi.pscnotifications.mappers;
 
-public class LinksMapperTest {
+import org.junit.jupiter.api.Test;
+import uk.gov.companieshouse.api.psc_notifications.NotificationListLinkTypes;
+import uk.gov.companieshouse.pscdataapi.models.Links;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+class LinksMapperTest {
+
+    private final LinksMapper mapper = new LinksMapper();
+
+    @Test
+    void testLinksMapper_returnsNotificationListLinkTypes_whenGivenValidInput() {
+        Links links = new Links();
+        links.setSelf("self-123");
+
+        NotificationListLinkTypes mappedLinks = mapper.map(links);
+
+        assertSame("self-123", mappedLinks.getSelf());
+    }
+
+    @Test
+    void testLinksMapper_returnsNull_whenGivenNullInput() {
+        assertNull(mapper.map(null));
+    }
 }
