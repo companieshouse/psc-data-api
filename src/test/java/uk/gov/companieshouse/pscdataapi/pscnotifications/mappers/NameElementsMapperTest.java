454a33dd7b0bd3c31a42359b0b591e9d9bac9948
brdywltn <bwilton@companieshouse.gov.uk>
2026-05-21 15:57:18 +0100

    Implement NameElementsMapper tests

--- src/test/java/uk/gov/companieshouse/pscdataapi/pscnotifications/mappers/NameElementsMapperTest.java
+++ src/test/java/uk/gov/companieshouse/pscdataapi/pscnotifications/mappers/NameElementsMapperTest.java
@@ -1,4 +1,35 @@
 package uk.gov.companieshouse.pscdataapi.pscnotifications.mappers;
 
-public class NameElementsMapperTest {
+import org.junit.jupiter.api.Test;
+import uk.gov.companieshouse.api.psc_notifications.NameElements;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+class NameElementsMapperTest {
+
+    private final NameElementsMapper mapper = new NameElementsMapper();
+
+    @Test
+    void testNameElementsMapper_returnsNameElements_whenGivenValidInput() {
+        uk.gov.companieshouse.pscdataapi.models.NameElements elements =
+                new uk.gov.companieshouse.pscdataapi.models.NameElements();
+
+        elements.setForename("Steve");
+        elements.setMiddleName("Bing");
+        elements.setSurname("Madden");
+        elements.setTitle("Mr");
+
+        NameElements mappedNameElements = mapper.map(elements);
+
+        assertSame("Steve", mappedNameElements.getForename());
+        assertSame("Bing", mappedNameElements.getMiddleName());
+        assertSame("Madden", mappedNameElements.getSurname());
+        assertSame("Mr", mappedNameElements.getTitle());
+    }
+
+    @Test
+    void testNameElementsMapper_returnsNull_whenGivenNullInput() {
+        assertNull(mapper.map(null));
+    }
 }
