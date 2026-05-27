5263ee5a1596a16ba5f5916f3eae7086735ae24e
brdywltn <bwilton@companieshouse.gov.uk>
2026-05-27 16:54:25 +0100

    Change repository methods to correctly use psc id

--- src/main/java/uk/gov/companieshouse/pscdataapi/pscnotifications/PscNotificationsRepository.java
+++ src/main/java/uk/gov/companieshouse/pscdataapi/pscnotifications/PscNotificationsRepository.java
@@ -1,23 +1,17 @@
 package uk.gov.companieshouse.pscdataapi.pscnotifications;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 import uk.gov.companieshouse.pscdataapi.models.PscDocument;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 interface PscNotificationsRepository extends MongoRepository<PscDocument, String> {
 
     List<PscDocument> findAll();
 
-    Optional<PscDocument> findById(String pscId);
+   List <PscDocument> findAllByPscId(String pscId);
 
-    @Query(value = "{ $and: [ "
-            + "{'psc_id': ?0 },"
-            + "{ 'company_status': { $nin: ?2 } }"
-            + "] }", count = true)
-    int countTotal(String pscId);
+    int countByPscId(String pscId);
 }
