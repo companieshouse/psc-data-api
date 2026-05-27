e8a95f4abcd938d1d64f0344f139d49e9f24ab7f
brdywltn <bwilton@companieshouse.gov.uk>
2026-05-27 16:57:33 +0100

    Add in the firstNotification to stop service returning 404 by default

--- src/main/java/uk/gov/companieshouse/pscdataapi/pscnotifications/PscNotificationsService.java
+++ src/main/java/uk/gov/companieshouse/pscdataapi/pscnotifications/PscNotificationsService.java
@@ -2,17 +2,21 @@ package uk.gov.companieshouse.pscdataapi.pscnotifications;
 
 import org.springframework.stereotype.Service;
 import uk.gov.companieshouse.api.psc_notifications.NotificationList;
+import uk.gov.companieshouse.logging.Logger;
+import uk.gov.companieshouse.logging.LoggerFactory;
+import uk.gov.companieshouse.pscdataapi.PscDataApiApplication;
 import uk.gov.companieshouse.pscdataapi.models.PscDocument;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+
 
 @Service
 public class PscNotificationsService {
 
     private final PscNotificationsRepository repository;
     private final PscNotificationsMapper mapper;
+    private static final Logger LOGGER = LoggerFactory.getLogger(PscDataApiApplication.APPLICATION_NAME_SPACE);
 
     PscNotificationsService(PscNotificationsRepository repository,
                             PscNotificationsMapper mapper) {
@@ -25,13 +29,15 @@ public class PscNotificationsService {
         final int startIndex = params.startIndex();
         final int itemsPerPage = params.itemsPerPage();
 
-        final int totalResults = repository.countTotal(pscId);
-        List<PscDocument> documents = repository.findAllById(Collections.singleton(pscId));
-
+        final int totalResults = repository.countByPscId(pscId);
+        LOGGER.info("Trying to find a list of PscDocuments by the pscId");
+        List<PscDocument> documents = repository.findAllByPscId(pscId);
+        PscDocument firstNotification = documents.isEmpty() ? null : documents.getFirst();
 
         return mapper.mapPscNotifications(PscNotificationsMapper.MapperRequest.builder()
                 .startIndex(startIndex)
                 .itemsPerPage(itemsPerPage)
+                .firstNotification(firstNotification)
                 .pscNotifications(documents)
                 .totalResults(totalResults)
                 .build());
