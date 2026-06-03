1a73754760b680097688afa400f93f6c368a4872
brdywltn <bwilton@companieshouse.gov.uk>
2026-06-03 09:27:23 +0100

    Implement SortingThresholdService

--- src/main/java/uk/gov/companieshouse/pscdataapi/pscnotifications/SortingThresholdService.java
+++ src/main/java/uk/gov/companieshouse/pscdataapi/pscnotifications/SortingThresholdService.java
@@ -1,4 +1,24 @@
 package uk.gov.companieshouse.pscdataapi.pscnotifications;
 
+import static uk.gov.companieshouse.pscdataapi.interceptor.AuthenticationHelperImpl.hasInternalAppPrivileges;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
 public class SortingThresholdService {
+
+    private final int internalSortingThreshold;
+    private final int externalSortingThreshold;
+
+    SortingThresholdService(@Value("500") final int internalSortingThreshold,
+                            @Value("500") final int externalSortingThreshold) {
+        this.internalSortingThreshold = internalSortingThreshold;
+        this.externalSortingThreshold = externalSortingThreshold;
+    }
+
+    boolean shouldSortByActiveThenCeased(int totalResults, String authPrivileges) {
+        int sortingThreshold = hasInternalAppPrivileges(authPrivileges) ? internalSortingThreshold : externalSortingThreshold;
+        return sortingThreshold == -1 || totalResults <= sortingThreshold;
+    }
 }
