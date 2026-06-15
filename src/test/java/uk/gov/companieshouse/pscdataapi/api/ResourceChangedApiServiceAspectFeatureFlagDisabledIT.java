ae5c2fcc0a72f2a99e8573749d6e97f97becbbe0
rmcmurray-ch <rmcmurray@companieshouse.gov.uk>
2026-06-15 12:33:10 +0100

    fix pre-existing NullPointerException in delete test

--- src/test/java/uk/gov/companieshouse/pscdataapi/api/ResourceChangedApiServiceAspectFeatureFlagDisabledIT.java
+++ src/test/java/uk/gov/companieshouse/pscdataapi/api/ResourceChangedApiServiceAspectFeatureFlagDisabledIT.java
@@ -1,12 +1,15 @@
 package uk.gov.companieshouse.pscdataapi.api;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.function.Supplier;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
@@ -20,10 +23,11 @@ import uk.gov.companieshouse.api.chskafka.ChangedResource;
 import uk.gov.companieshouse.api.error.ApiErrorResponseException;
 import uk.gov.companieshouse.api.handler.chskafka.PrivateChangedResourceHandler;
 import uk.gov.companieshouse.api.handler.chskafka.request.PrivateChangedResourcePost;
-import uk.gov.companieshouse.api.http.HttpClient;
 import uk.gov.companieshouse.api.model.ApiResponse;
+import uk.gov.companieshouse.api.psc.Individual;
 import uk.gov.companieshouse.api.sdk.ApiClientService;
 import uk.gov.companieshouse.pscdataapi.models.PscDeleteRequest;
+import uk.gov.companieshouse.pscdataapi.transform.CompanyPscTransformer;
 import uk.gov.companieshouse.pscdataapi.util.TestHelper;
 
 @SpringBootTest
@@ -37,15 +41,11 @@ class ResourceChangedApiServiceAspectFeatureFlagDisabledIT {
 
     @MockitoBean
     private ApiClientService apiClientService;
-    @MockitoBean
-    private ChsKafkaApiService mapper;
 
     @Mock
     private Supplier<InternalApiClient> kafkaApiClientSupplier;
     @Mock
     private InternalApiClient client;
-    @Mock
-    private ChangedResource changedResource;
     @Mock
     private PrivateChangedResourceHandler privateChangedResourceHandler;
     @Mock
@@ -53,9 +53,9 @@ class ResourceChangedApiServiceAspectFeatureFlagDisabledIT {
     @Mock
     private ApiResponse<Void> response;
     @Mock
-    private HttpClient httpClient;
-    @Mock
     private ObjectMapper objectMapper;
+    @Mock
+    private CompanyPscTransformer companyPscTransformer;
 
     @Test
     void testThatKafkaApiShouldBeCalledWhenFeatureFlagDisabled() throws ApiErrorResponseException {
@@ -69,7 +69,7 @@ class ResourceChangedApiServiceAspectFeatureFlagDisabledIT {
         ApiResponse<?> apiResponse = chsKafkaApiService.invokeChsKafkaApi(TestHelper.COMPANY_NUMBER, TestHelper.NOTIFICATION_ID,
                 "individual-person-with-significant-control");
 
-        Assertions.assertThat(apiResponse).isNotNull();
+        assertThat(apiResponse).isNotNull();
 
         verify(client).privateChangedResourceHandler();
         verify(privateChangedResourceHandler, times(1)).postChangedResource(Mockito.any(), changedResourceCaptor.capture());
@@ -77,7 +77,7 @@ class ResourceChangedApiServiceAspectFeatureFlagDisabledIT {
     }
 
     @Test
-    void testThatKafkaApiShouldBeCalledOnDeleteWhenFeatureFlagDisabled() throws ApiErrorResponseException {
+    void testThatKafkaApiShouldBeCalledOnDeleteWhenFeatureFlagDisabled() throws ApiErrorResponseException, JsonProcessingException {
         when(kafkaApiClientSupplier.get()).thenReturn(client);
         when(client.privateChangedResourceHandler()).thenReturn(
                 privateChangedResourceHandler);
@@ -85,15 +85,27 @@ class ResourceChangedApiServiceAspectFeatureFlagDisabledIT {
                 changedResourcePost);
         when(changedResourcePost.execute()).thenReturn(response);
 
+        Individual individual = new Individual();
+        individual.setKind(Individual.KindEnum.INDIVIDUAL_PERSON_WITH_SIGNIFICANT_CONTROL);
+        when(companyPscTransformer.transformPscDocToIndividual(any(), eq(false))).thenReturn(individual);
+        when(objectMapper.writeValueAsString(individual)).thenReturn(individual.toString());
+        when(objectMapper.readValue(individual.toString(), Object.class)).thenReturn(individual);
+
         ApiResponse<?> apiResponse = chsKafkaApiService.invokeChsKafkaApiWithDeleteEvent(
                 new PscDeleteRequest(TestHelper.X_REQUEST_ID, TestHelper.COMPANY_NUMBER, TestHelper.NOTIFICATION_ID,
                         "individual-person-with-significant-control", "deltaAt"),
-                TestHelper.buildPscDocument("individual-persons-with-significant-control"));
+                TestHelper.buildPscDocument("individual-person-with-significant-control"));
 
-        Assertions.assertThat(apiResponse).isNotNull();
+        assertThat(apiResponse).isNotNull();
 
         verify(client).privateChangedResourceHandler();
         verify(privateChangedResourceHandler, times(1)).postChangedResource(Mockito.any(), changedResourceCaptor.capture());
         verify(changedResourcePost, times(1)).execute();
+        verify(companyPscTransformer, times(2)).transformPscDocToIndividual(any(), eq(false));
+
+        ChangedResource captured = changedResourceCaptor.getValue();
+        assertThat(captured.getEvent().getType()).isEqualTo("deleted");
+        assertThat(captured.getDeletedData()).isInstanceOf(Individual.class);
+        assertThat(captured.getResourceKind()).isEqualTo("company-psc-individual");
     }
 }
