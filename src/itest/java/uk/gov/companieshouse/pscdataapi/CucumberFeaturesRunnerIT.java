b2fd9d41e20a36b7779b07aad092d49c3491ccc6
brdywltn <bwilton@companieshouse.gov.uk>
2026-07-09 09:48:16 +0100

    Update cucumber tests and config to account for new authenticationinterceptor class

--- src/itest/java/uk/gov/companieshouse/pscdataapi/CucumberFeaturesRunnerIT.java
+++ src/itest/java/uk/gov/companieshouse/pscdataapi/CucumberFeaturesRunnerIT.java
@@ -1,6 +1,10 @@
 package uk.gov.companieshouse.pscdataapi;
 
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+
 import io.cucumber.spring.CucumberContextConfiguration;
+import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
@@ -19,6 +23,8 @@ import uk.gov.companieshouse.pscdataapi.service.CompanyMetricsApiService;
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value="uk.gov.companieshouse.pscdataapi")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value="pretty")
 @CucumberContextConfiguration
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @DirtiesContext
